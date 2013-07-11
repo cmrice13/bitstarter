28beaef13a46636e8b76d97bfcfd29d2f5d5360f
Chris Rice <cmrice13@gmail.com>
2013-07-11 19:05:15 +0000

    Comment string and test of Buffer

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express  = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+var readIn = new Buffer(fs.readFileSync('index.html'));
+response.send(readIn.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
