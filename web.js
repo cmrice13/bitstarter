eefea1684239b87239dd97c3d13ab841c8a23511
cmrice13 <cmrice13@gmail.com>
2013-07-23 20:39:38 +0000

    Starting to add the new CSS/bootstrap stylesheets

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var readIn = new Buffer(fs.readFileSync('index.html'));
 response.send(readIn.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
